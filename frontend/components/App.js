e17fbe82f006e594fa8916d4227f8ea25f72dc7b
Hailey Murphy <foxfireevee@gmail.com>
2024-04-14 16:53:55 -0400

    Added Styling, and got the darkmode working.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,11 @@ import axios from "axios";
 import Image from "./image";
 
 export default function App() {
+  // State
   const [data, setData] = useState("");
+  const [darkmodeOn, setDarkmode] = useState(false);
+
+  // Fetching API
   useEffect(() => {
     function getAPI() {
       axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
@@ -17,10 +21,21 @@ export default function App() {
     getAPI();
   }, []);
 
+  // Callback Functions
+  // Changes dark mode on or off
+  const darkmode = () => {
+    if (!darkmodeOn) {
+      setDarkmode(true);
+    } else if (darkmode) {
+      setDarkmode(false);
+    }
+  };
 
+
+  
   return (
     <div>
-      <Image data={data}/>
+      <Image data={data} darkmode={darkmode} darkmodeOn={darkmodeOn}/>
     </div>
   )
 }
--- frontend/components/image.js
+++ frontend/components/image.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`
-        color: ${pr => pr.theme.secondaryColor};
-        border: 1px solid red;
-`
+const footer = document.querySelector('footer');
 
-export default function image(props) {
-    const { data } = props;
-
+export default function image({ data, darkmode, darkmodeOn }) {
+    const StyledDiv = styled.div`
+        background-color: ${pr => darkmodeOn ? pr.theme.black : pr.theme.white};
+        h1 {
+            color: ${pr => darkmodeOn ? pr.theme.white : pr.theme.black};
+        }
+        h2 {
+            color: ${pr => darkmodeOn ? pr.theme.secondaryColor : pr.theme.black};
+        }
+        h3 {
+            color: ${pr => darkmodeOn ? pr.theme.primaryColor : pr.theme.black};
+        }
+        p {
+            color: ${pr => darkmodeOn ? pr.theme.primaryColor : pr.theme.black};
+        }
+        img {
+            border: 1px solid ${pr => darkmodeOn ? pr.theme.white : pr.theme.black};
+            border-radius: 5%;
+        };
+        button, select, option {
+            background-color: ${pr => darkmodeOn ? pr.theme.tertiaryColor : pr.theme.secondaryColor};
+            color: ${pr => darkmodeOn ? pr.theme.secondaryColor : pr.theme.black}
+        }
+    `
+    //Dates and past Dates
     const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const newDate = new Date(data.date); // Assuming "data.date" is in the format "YYYY-MM-DD".
@@ -38,25 +57,39 @@ export default function image(props) {
 
     console.log(pastDates);
 
+    // Honoring the Copyright artist 
+    const copyrightArist = () => {
+        if (!data.copyright) {
+            return 'Anonymous';
+        } else {
+            return data.copyright;
+        }
+    }
 
-
-    console.log(data)
+    console.log(copyrightArist())
     return (
         <StyledDiv className='centered'>
-            <select>
-                <option selected disabled hidden>Select a Different Day!</option>
-                {
-                    pastDates.map(date => {
-                        return (
-                            <option>{date}</option>
-                        )
-                    })
-                }
-            </select>
+            <header>
+                <h1>NASA Photo of the Day</h1>
+            </header>
+            <div>
+               <select>
+                    <option selected disabled hidden>Select a Different Day!</option>
+                    {
+                        pastDates.map(date => {
+                            return (
+                                <option>{date}</option>
+                            )
+                        })
+                    }
+                </select> 
+                <button onClick={() => darkmode()}>Temporary Button Name</button>   
+            </div>
+            
             <h2>{data.title}</h2>
             <p>{activeDate}</p>
             <img src={data.url} />
-            <h3>Shout out to {data.copyright} for this out of this world photo!</h3>
+            <h3>Shout out to {copyrightArist()} for this out of this world photo!</h3>
             <p>{data.explanation}</p>
         </StyledDiv>
     )
